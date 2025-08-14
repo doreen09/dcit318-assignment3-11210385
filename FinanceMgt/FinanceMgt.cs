123e5e1624303432757a902005958369ea03bd4d
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-14 17:46:24 +0000

    created three more concrete classes, with each providing a distinct  implemntation of a method and printing a message

--- FinanceMgt/FinanceMgt.cs
+++ FinanceMgt/FinanceMgt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace FinanceManagementSystem
 {
- 
+
     public record Transaction(
         int Id,
         DateTime Date,
@@ -11,9 +11,33 @@ namespace FinanceManagementSystem
         string Category
     );
 
- 
+
     public interface ITransactionProcessor
     {
         void Process(Transaction transaction);
+    }
+    
+     public class BankTransferProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[BankTransfer] Processing {transaction.Category} for {transaction.Amount:C} on {transaction.Date:d}.");
+        }
+    }
+
+    public class MobileMoneyProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[MobileMoney] Sent {transaction.Amount:C} for {transaction.Category} on {transaction.Date:d}.");
+        }
+    }
+
+    public class CryptoWalletProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[CryptoWallet] Debited {transaction.Amount:C} for {transaction.Category} on {transaction.Date:d} (network fee included).");
+        }
     }
 }
