da8c3aab59aa7da3bea23eadeefa0e39c5610880
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-14 16:43:54 +0000

    Created a record type to represent financial data

---
+++ FinanceMgt/FinanceMgt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinanceManagementSystem.Models
+{
+    public record Transaction(
+        int Id,
+        DateOnly Date,
+        decimal Amount,
+        string Category
+    );
+}
