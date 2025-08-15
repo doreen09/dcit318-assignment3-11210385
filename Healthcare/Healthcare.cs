485a2b5c698ae09950b0c12815eda078f64c0e34
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 16:27:34 +0000

    Created a repository that will be used to manage both patients and prescriptions

---
+++ Healthcare/Healthcare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HealthcareSystem
+{
+    //Created a repository that will be used to manage both patients and prescriptions
+    public class Repository<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item) => items.Add(item);
+
+        public List<T> GetAll() => items;
+
+        public T? GetById(Func<T, bool> predicate) => items.FirstOrDefault(predicate);
+
+        public bool Remove(Func<T, bool> predicate)
+        {
+            var item = items.FirstOrDefault(predicate);
+            if (item != null)
+            {
+                items.Remove(item);
+                return true;
+            }
+            return false;
+        }
+    }
+}
