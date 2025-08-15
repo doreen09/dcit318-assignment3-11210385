fcf12849cbe0d64c3392360cce539b760baa5c11
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 14:29:38 +0000

    edited the whole file and created a record type to represent financial data

--- FinanceMgt/FinanceMgt.cs
+++ FinanceMgt/FinanceMgt.cs
@@ -3,41 +3,11 @@ using System.Collections.Generic;
 
 namespace FinanceManagementSystem
 {
-
     public record Transaction(
         int Id,
         DateTime Date,
         decimal Amount,
         string Category
-    );
+        );
 
-
-    public interface ITransactionProcessor
-    {
-        void Process(Transaction transaction);
-    }
-    
-     public class BankTransferProcessor : ITransactionProcessor
-    {
-        public void Process(Transaction transaction)
-        {
-            Console.WriteLine($"[BankTransfer] Processing {transaction.Category} for {transaction.Amount:C} on {transaction.Date:d}.");
-        }
-    }
-
-    public class MobileMoneyProcessor : ITransactionProcessor
-    {
-        public void Process(Transaction transaction)
-        {
-            Console.WriteLine($"[MobileMoney] Sent {transaction.Amount:C} for {transaction.Category} on {transaction.Date:d}.");
-        }
-    }
-
-    public class CryptoWalletProcessor : ITransactionProcessor
-    {
-        public void Process(Transaction transaction)
-        {
-            Console.WriteLine($"[CryptoWallet] Debited {transaction.Amount:C} for {transaction.Category} on {transaction.Date:d} (network fee included).");
-        }
-    }
 }
