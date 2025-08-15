f0798143fed26b47903e6ec94cfd35acc14e2d24
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 18:21:27 +0000

    Created a student class with a get method to get student grades

--- GradingSystem/GradingSystem.cs
+++ GradingSystem/GradingSystem.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SchoolGradingSystem
+{
+    // a) Student class
+    public class Student
+    {
+        public int Id;
+        public string FullName;
+        public int Score;
+
+        public Student(int id, string fullName, int score)
+        {
+            Id = id;
+            FullName = fullName;
+            Score = score;
+        }
+
+        public string GetGrade()
+        {
+            if (Score >= 80 && Score <= 100) return "A";
+            if (Score >= 70 && Score <= 79) return "B";
+            if (Score >= 60 && Score <= 69) return "C";
+            if (Score >= 50 && Score <= 59) return "D";
+            return "F";
+        }
+    }
+
+
+
+
+
+}
