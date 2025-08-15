62fad1b7a2244da87755851854b24f2eb5029c04
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 19:12:05 +0000

    Created the IInventoryEntity interface for an Id property and implemented the InventoryItem record to include Name, Quantity, and DateAdded properties.

--- InventoryRecord/InventoryRecord.cs
+++ InventoryRecord/InventoryRecord.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public interface IInventoryEntity
+{
+    int Id { get; }
+}
+
+public record InventoryItem(
+    int Id,
+    string Name,
+    int Quantity,
+    DateTime DateAdded) : IInventoryEntity;
+
