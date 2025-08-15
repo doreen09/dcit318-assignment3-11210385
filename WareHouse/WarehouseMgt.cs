0e48abd0ea22d4329faed8b7eb72ff37dc238522
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 17:16:08 +0000

    Created an interface for Inventory items

---
+++ WareHouse/WarehouseMgt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarehouseInventorySystem
+{
+    public interface IInventoryItem
+    {
+        int Id { get; }
+        string Name { get; }
+        int Quantity { get; set; }
+    }
+
+
+
+
+}
