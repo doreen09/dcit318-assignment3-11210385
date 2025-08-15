1580f78f605a527e7a199660a403143003ec8688
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 14:49:07 +0000

    created three more concrete classes, with each providing a distinct  implemntation of a method and printing a message

--- FinanceMgt/FinanceMgt.cs
+++ FinanceMgt/FinanceMgt.cs
@@ -10,9 +10,35 @@ namespace FinanceManagementSystem
         string Category
         );
 
-      public interface ITransactionProcessor
+//created an interface
+    public interface ITransactionProcessor
     {
         void Process(Transaction transaction);
+    }
+    
+//Created three concrete classes implementing the interface
+      public class BankTransferProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[BankTransfer] {transaction.Category}: {transaction.Amount:C}");
+        }
+    }
+
+    public class MobileMoneyProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[MobileMoney] {transaction.Category}: {transaction.Amount:C}");
+        }
+    }
+
+    public class CryptoWalletProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[CryptoWallet] {transaction.Category}: {transaction.Amount:C}");
+        }
     }
 
 }
