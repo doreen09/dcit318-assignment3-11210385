04ea7b4d807c4facdd891182b311971aa68e7c2f
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 19:33:25 +0000

    final commit

--- InventoryRecord/InventoryRecord.cs
+++ InventoryRecord/InventoryRecord.cs
@@ -72,3 +72,58 @@ public class InventoryLogger<T> where T : IInventoryEntity
     }
 }
 
+public class InventoryApp
+{
+    private InventoryLogger<InventoryItem> _logger;
+
+    public InventoryApp(string filePath)
+    {
+        _logger = new InventoryLogger<InventoryItem>(filePath);
+    }
+
+    public void SeedSampleData()
+    {
+        _logger.Add(new InventoryItem(1, "Laptop", 5, DateTime.Now));
+        _logger.Add(new InventoryItem(2, "Mouse", 10, DateTime.Now));
+        _logger.Add(new InventoryItem(3, "Keyboard", 7, DateTime.Now));
+        _logger.Add(new InventoryItem(4, "Monitor", 3, DateTime.Now));
+        _logger.Add(new InventoryItem(5, "Printer", 2, DateTime.Now));
+    }
+
+    public void SaveData()
+    {
+        _logger.SaveToFile();
+    }
+
+    public void LoadData()
+    {
+        _logger.LoadFromFile();
+    }
+
+    public void PrintAllItems()
+    {
+        var items = _logger.GetAll();
+        foreach (var item in items)
+        {
+            Console.WriteLine($"ID: {item.Id}, Name: {item.Name}, Quantity: {item.Quantity}, Date Added: {item.DateAdded}");
+        }
+    }
+}
+
+class InventoryRecord 
+{
+    static void Main()
+    {
+        string filePath = "inventory.json";
+
+        InventoryApp app = new InventoryApp(filePath);
+        app.SeedSampleData();
+        app.SaveData();
+
+        Console.WriteLine("\n--- Simulating new session ---\n");
+        InventoryApp newApp = new InventoryApp(filePath);
+        newApp.LoadData();
+        newApp.PrintAllItems();
+    }
+}
+
