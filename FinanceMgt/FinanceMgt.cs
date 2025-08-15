cff0217bb04d1096448fc034f7ae47cda02bbd20
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 14:58:54 +0000

    created a General Account and Seal a Specialized Account

--- FinanceMgt/FinanceMgt.cs
+++ FinanceMgt/FinanceMgt.cs
@@ -10,14 +10,14 @@ namespace FinanceManagementSystem
         string Category
         );
 
-//created an interface
+    //created an interface
     public interface ITransactionProcessor
     {
         void Process(Transaction transaction);
     }
-    
-//Created three concrete classes implementing the interface
-      public class BankTransferProcessor : ITransactionProcessor
+
+    //Created three concrete classes implementing the interface
+    public class BankTransferProcessor : ITransactionProcessor
     {
         public void Process(Transaction transaction)
         {
@@ -41,4 +41,41 @@ namespace FinanceManagementSystem
         }
     }
 
+
+    //created a General Account and Seal a Specialized Account
+    public class Account
+    {
+        public string AccountNumber { get; }
+        public decimal Balance { get; protected set; }
+
+        public Account(string accountNumber, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public virtual void ApplyTransaction(Transaction transaction)
+        {
+            Balance -= transaction.Amount;
+        }
+    }
+    
+      public sealed class SavingsAccount : Account
+    {
+        public SavingsAccount(string accountNumber, decimal initialBalance)
+            : base(accountNumber, initialBalance) { }
+
+        public override void ApplyTransaction(Transaction transaction)
+        {
+            if (transaction.Amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+            else
+            {
+                Balance -= transaction.Amount;
+                Console.WriteLine($"Updated balance: {Balance:C}");
+            }
+        }
+    }
 }
