7847a106c8c1587bff51d58f92226063dcce5f18
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-15 17:26:21 +0000

    Created product classes that implement IInventoryItem

--- WareHouse/WarehouseMgt.cs
+++ WareHouse/WarehouseMgt.cs
@@ -10,6 +10,40 @@ namespace WarehouseInventorySystem
         int Quantity { get; set; }
     }
 
+    public class ElectronicItem : IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public string Brand { get; }
+        public int WarrantyMonths { get; }
+
+        public ElectronicItem(int id, string name, int quantity, string brand, int warrantyMonths)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Brand = brand;
+            WarrantyMonths = warrantyMonths;
+        }
+    }
+
+    public class GroceryItem : IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; }
+
+        public GroceryItem(int id, string name, int quantity, DateTime expiryDate)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            ExpiryDate = expiryDate;
+        }
+    }
+
 
 
 
